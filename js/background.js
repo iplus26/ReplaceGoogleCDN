db1e4067af67a77c2c9b53ca97b60cce8258a9b1
iplus26 <iplus26@gmail.com>
2015-08-02 19:26:49 +0800

    add v2ex cdn for gravatar

--- js/background.js
+++ js/background.js
@@ -1,15 +1,21 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(request) {
-        var url = request.url.replace('googleapis.com', 'lug.ustc.edu.cn');
-        url = url.replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn');
-        return {redirectUrl: url};
-    },
+'use strict';
+
+chrome.webRequest.onBeforeRequest.addListener(function(request) {
+    var url = request.url
+        .replace('googleapis.com', 'lug.ustc.edu.cn')
+        .replace('themes.googleusercontent.com', 'google-themes.lug.ustc.edu.cn')
+        .replace(/(www|\d).gravatar.com/, 'cdn.v2ex.com');
+    return {
+        redirectUrl: url
+    };
+},
     {
         urls: [
-            "*://ajax.googleapis.com/*",
-            "*://fonts.googleapis.com/*",
-            "*://themes.googleusercontent.com/*"
+            '*://ajax.googleapis.com/*',
+            '*://fonts.googleapis.com/*',
+            '*://themes.googleusercontent.com/*',
+            '*://*.gravatar.com/*'
         ]
     },
-    ["blocking"]
+    ['blocking']
 );
